5d24b6f3cdddef0c337dfd9b497e40205d7e6f2c
Kyann Brown <kyann.brown@my.simpson.edu>
2017-01-19 15:40:56 -0600

    Fixing file structor again

--- javascript_assignment.js
+++ javascript_assignment.js
@@ -1,3 +1,6 @@
+/**
+ * Created by kyann.brown on 1/19/2017..
+ */
 //Part 1
 function printFunction(event)
 {
@@ -36,3 +39,39 @@ function hideFunction(event)
 
 var hideButton = $('#button3');
 hideButton.on("click", hideFunction);
+
+//Part 4
+function correctPhoneNumber(event)
+{
+    var phoneNumber = $('#phoneField').val();
+    var regularExpression = /^\d{3}-\d{3}-\d{4}$/;
+
+    if (regularExpression.test(phoneNumber))
+    {
+        console.log("OK");
+    }
+    else
+    {
+        console.log("Bad");
+    }
+}
+
+var phoneButton = $('#button4');
+phoneButton.on("click", correctPhoneNumber);
+
+//Part 5
+function jsonFunction(event)
+{
+    var jsonObject = {};
+
+    jsonObject.firstName = $('#firstName').val();
+    jsonObject.lastName = $('#lastName').val();
+    jsonObject.email = $('#email').val();
+
+    var jsonString = JSON.stringify(jsonObject);
+
+    console.log(jsonObject);
+}
+
+var jsonButton = $('#button5');
+jsonButton.on("click", jsonFunction);
