edf02774aa82e648f10c736ee57768b7edb8a2af
Kyann Brown <kyann.brown@my.simpson.edu>
2017-02-16 09:29:10 -0600

    Inserting data from form

--- src/edu/simpson/brown/NameListEdit.java
+++ src/edu/simpson/brown/NameListEdit.java
@@ -8,6 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
 /**
  * Created by kyann.brown on 2/14/2017.
  */
@@ -18,5 +28,48 @@ public class NameListEdit extends HttpServlet {
         PrintWriter out = response.getWriter();
 
         out.println("JSON Post is working");
+
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+        String birthday = request.getParameter("birthday");
+
+
+        Logger log = Logger.getLogger(DBHelper.class.getName());
+
+        out.println(firstName);
+        out.println(lastName);
+        out.println(email);
+        out.println(phone);
+        out.println(birthday);
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+    try
+    {
+        conn = DBHelper.getConnection();
+
+        String sql = "INSERT INTO person (first, last, email, phone, birthday) VALUES(?, ?, ?, ?, ?);";
+
+        stmt = conn.prepareStatement(sql);
+
+        stmt.setString(1, firstName);
+        stmt.setString(2, lastName);
+        stmt.setString(3, email);
+        stmt.setString(4, phone);
+        stmt.setString(5, birthday);
+
+        stmt.executeUpdate();
+    }
+    catch(SQLException se) {
+        log.log(Level.SEVERE, "SQL Error", se );
+    } catch (Exception e) {
+        log.log(Level.SEVERE, "Error", e );
+    } finally {
+        try { stmt.close(); } catch (Exception e) { log.log(Level.SEVERE, "Error", e ); }
+        try { conn.close(); } catch (Exception e) { log.log(Level.SEVERE, "Error", e ); }
+    }
     }
 }
