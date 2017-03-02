dd04d5f5ec35bde0fba23fcbd123cdd867a6dfa0
Kyann Brown <kyann.brown@my.simpson.edu>
2017-03-02 09:31:40 -0600

    Working on new sernlet

--- src/edu/simpson/brown/PersonDAO.java
+++ src/edu/simpson/brown/PersonDAO.java
@@ -93,4 +93,31 @@ public class PersonDAO
             try { conn.close(); } catch (Exception e) { log.log(Level.SEVERE, "Error", e ); }
         }
     }
+
+    public static void deletePerson (int id) {
+        Logger log = Logger.getLogger(DBHelper.class.getName());
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            conn = DBHelper.getConnection();
+
+            String sql = "DELETE FROM person WHERE id = ;";
+
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, id);
+
+            stmt.executeUpdate();
+
+        } catch (SQLException se) {
+            log.log(Level.SEVERE, "SQL Error", se);
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "Error", e);
+        } finally {
+            try {stmt.close();} catch (Exception e) {log.log(Level.SEVERE, "Error", e);}
+            try {conn.close();} catch (Exception e) {log.log(Level.SEVERE, "Error", e);}
+        }
+    }
 }
