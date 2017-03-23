8b27d13efc17cf0c5d5466a793aa07c0e5da9cc4
Kyann Brown <kyann.brown@my.simpson.edu>
2017-03-23 09:04:36 -0500

    Added all html, js, and servlet files and renamed

---
+++ src/edu/simpson/brown/LoginServlet.java
@@ -0,0 +1,30 @@
+package edu.simpson.brown;
+
+/**
+ * Created by kyann.brown on 3/23/2017.
+ */
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "SetSessionServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+
+        String sessionKey = request.getParameter("sessionKey");
+        String sessionValue = request.getParameter("sessionValue");
+
+        HttpSession session = request.getSession();
+        session.setAttribute(sessionKey, sessionValue);
+
+        out.println("Done setting the session variable");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
